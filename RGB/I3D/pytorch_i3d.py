9a7b8903a3ee077189db3e019cab400002e1f992
kimslor <klor742@aucklanduni.ac.nz>
2022-06-30 17:52:11 +1200

    Added dataset and config scripts.

--- RGB/I3D/pytorch_i3d.py
+++ RGB/I3D/pytorch_i3d.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class MaxPool3dSamePadding(nn.MaxPool3d):
-    
+
     def compute_pad(self, dim, s):
         if s % self.stride[dim] == 0:
             return max(self.kernel_size[dim] - self.stride[dim], 0)
@@ -38,7 +38,7 @@ class MaxPool3dSamePadding(nn.MaxPool3d):
         #print pad
         x = F.pad(x, pad)
         return super(MaxPool3dSamePadding, self).forward(x)
-    
+
 
 class Unit3D(nn.Module):
 
@@ -51,10 +51,10 @@ class Unit3D(nn.Module):
                  use_batch_norm=True,
                  use_bias=False,
                  name='unit_3d'):
-        
+
         """Initializes Unit3D module."""
         super(Unit3D, self).__init__()
-        
+
         self._output_channels = output_channels
         self._kernel_shape = kernel_shape
         self._stride = stride
@@ -63,14 +63,14 @@ class Unit3D(nn.Module):
         self._use_bias = use_bias
         self.name = name
         self.padding = padding
-        
+
         self.conv3d = nn.Conv3d(in_channels=in_channels,
                                 out_channels=self._output_channels,
                                 kernel_size=self._kernel_shape,
                                 stride=self._stride,
                                 padding=0, # we always want padding to be 0 here. We will dynamically pad based on input size in forward function
                                 bias=self._use_bias)
-        
+
         if self._use_batch_norm:
             self.bn = nn.BatchNorm3d(self._output_channels, eps=0.001, momentum=0.01)
 
@@ -80,7 +80,7 @@ class Unit3D(nn.Module):
         else:
             return max(self._kernel_shape[dim] - (s % self._stride[dim]), 0)
 
-            
+
     def forward(self, x):
         # compute 'same' padding
         (batch, channel, t, h, w) = x.size()
@@ -105,7 +105,7 @@ class Unit3D(nn.Module):
         #print x.size()
         #print pad
         x = F.pad(x, pad)
-        #print x.size()        
+        #print x.size()
 
         x = self.conv3d(x)
         if self._use_batch_norm:
@@ -136,7 +136,7 @@ class InceptionModule(nn.Module):
                           name=name+'/Branch_3/Conv3d_0b_1x1')
         self.name = name
 
-    def forward(self, x):    
+    def forward(self, x):
         b0 = self.b0(x)
         b1 = self.b1b(self.b1a(x))
         b2 = self.b2b(self.b2a(x))
@@ -221,17 +221,17 @@ class InceptionI3d(nn.Module):
         self.end_points[end_point] = Unit3D(in_channels=in_channels, output_channels=64, kernel_shape=[7, 7, 7],
                                             stride=(2, 2, 2), padding=(3,3,3),  name=name+end_point)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'MaxPool3d_2a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
                                                              padding=0)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Conv3d_2b_1x1'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=64, kernel_shape=[1, 1, 1], padding=0,
                                        name=name+end_point)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Conv3d_2c_3x3'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=192, kernel_shape=[3, 3, 3], padding=1,
                                        name=name+end_point)
@@ -241,7 +241,7 @@ class InceptionI3d(nn.Module):
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
                                                              padding=0)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Mixed_3b'
         self.end_points[end_point] = InceptionModule(192, [64,96,128,16,32,32], name+end_point)
         if self._final_endpoint == end_point: return
@@ -303,7 +303,7 @@ class InceptionI3d(nn.Module):
 
     def replace_logits(self, num_classes):
         self._num_classes = num_classes
-        self.logits = Unit3D(in_channels=384+384+128+128, 
+        self.logits = Unit3D(in_channels=384+384+128+128,
                              output_channels=self._num_classes,
                              kernel_shape=[1, 1, 1],
                              padding=0,
@@ -315,7 +315,7 @@ class InceptionI3d(nn.Module):
     def build(self):
         for k in self.end_points.keys():
             self.add_module(k, self.end_points[k])
-        
+
     def forward(self, x, pretrained=False, n_tune_layers=-1):
         if pretrained:
             assert n_tune_layers >= 0
---
+++ RGB/I3D/train_i3d.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+def train_i3d(
+    config_path: str,
+    mode: str = 'rgb',
+    ) -> None:
+
