705f9a6c03a18b3dc64b0ff52dfeb09f6e55a48b
Kimsong <kimslor@hotmail.com>
2022-07-04 22:49:59 +1200

    Added msasl make dataset method.

--- RGB/I3D/dataset/wlasl_dataset_utilities.py
+++ RGB/I3D/dataset/wlasl_dataset_utilities.py
@@ -1,14 +1,9 @@
 import json
-import math
 import os
 import os.path
-import random
 
 import cv2
 import numpy as np
-import torch
-import torch.utils.data as data_utl
-from torchvision import transforms
 
 
 def wlasl_num_class(split_file: str) -> int:
@@ -28,7 +23,6 @@ def wlasl_make_dataset(split_file: str, split: str, root_dir: str, mode: str,
     with open(split_file, 'r') as f:
         data = json.load(f)
 
-    i = 0
     count_skipping = 0
     for vid in data.keys():
         if split == 'train':
@@ -68,8 +62,6 @@ def wlasl_make_dataset(split_file: str, split: str, root_dir: str, mode: str,
         elif len(vid) == 6:  # sign kws instances
             dataset.append((vid, label, src, data[vid]['action'][1],
                             data[vid]['action'][2] - data[vid]['action'][1]))
-
-        i += 1
 
     print("Skipped videos: ", count_skipping)
     print(len(dataset))
