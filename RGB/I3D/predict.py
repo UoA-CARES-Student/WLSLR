e239831e499b65bf4c43ed25ed9662c6b2d7317b
Kimsong <kimslor@hotmail.com>
2022-07-21 15:11:40 +1200

    reenabling msasl model search.

--- RGB/I3D/predict.py
+++ RGB/I3D/predict.py
@@ -4,19 +4,19 @@ import numpy as np
 import torchvision
 import skvideo
 skvideo.setFFmpegPath("C:/Users/isabe/Downloads/ffmpeg-5.0.1-full_build-shared/bin")
-import skvideo.io  
+import skvideo.io
 from pytorch_i3d import InceptionI3d
 
 if __name__ == "__main__":
     model = InceptionI3d(400, in_channels=3)
     model.replace_logits(100)
 
-    model.load_state_dict(torch.load("RGB/I3D/archived/asl100/FINAL_nslt_100_iters=896_top1=65.89_top5=84.11_top10=89.92.pt", 
+    model.load_state_dict(torch.load("RGB/I3D/archived/asl100/FINAL_nslt_100_iters=896_top1=65.89_top5=84.11_top10=89.92.pt",
         map_location=torch.device('cpu')))
     model = nn.DataParallel(model)
     model.eval()
 
-    data = skvideo.io.vread("RGB/data/test/book0.mp4") 
+    data = skvideo.io.vread("RGB/data/test/book0.mp4")
     data = torch.from_numpy(data.transpose([3, 0, 1, 2]))
     data.unsqueeze_(0)
     print(data.shape)
