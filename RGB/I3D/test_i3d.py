b64b6cb1ca2acece69d2253eacdca1a91b26a067
myuser1 <klor742@aucklanduni.ac.nz>
2022-07-21 16:09:12 +1200

    Modded testing to only check for top1.

--- RGB/I3D/test_i3d.py
+++ RGB/I3D/test_i3d.py
@@ -73,40 +73,20 @@ def test_i3d(
         pin_memory=False)
 
     # setup the model
-    if mode == 'flow':
-        i3d = InceptionI3d(400, in_channels=2)
-        weight_path = pathlib.Path(
-            WLSLR_GIT_PATH, "RGB", "I3D", "weights", "flow_imagenet.pt")
-        i3d.load_state_dict(torch.load(weight_path, map_location=torch.device('cpu')))
-    else:
-        i3d = InceptionI3d(400, in_channels=3)
-        weight_path = pathlib.Path(
-            WLSLR_GIT_PATH, "RGB", "I3D", "weights", "rgb_imagenet.pt")
-        i3d.load_state_dict(torch.load(weight_path, map_location=torch.device('cpu')))
-
+    i3d = InceptionI3d(400, in_channels=3)
     i3d.replace_logits(val_dataset.num_classes)
     trained_model = get_best_model(trained_models_dir=trained_models_dir)
-    i3d.load_state_dict(torch.load(trained_model, map_location=torch.device('cpu')))
-    # i3d.cuda()
+    i3d.load_state_dict(torch.load(trained_model))
+    i3d.cuda()
     i3d = nn.DataParallel(i3d)
     i3d.eval()
 
-    # Set up results containers
     correct = 0
-    correct_5 = 0
-    correct_10 = 0
-
-    top1_fp = np.zeros(val_dataset.num_classes, dtype=int)
-    top1_tp = np.zeros(val_dataset.num_classes, dtype=int)
-
-    top5_fp = np.zeros(val_dataset.num_classes, dtype=int)
-    top5_tp = np.zeros(val_dataset.num_classes, dtype=int)
-
-    top10_fp = np.zeros(val_dataset.num_classes, dtype=int)
-    top10_tp = np.zeros(val_dataset.num_classes, dtype=int)
-
+    count = 0
+    correct_vids = []
     # Test the model with the dataset
     for data in val_dataloader:
+        count = count + 1
         inputs, labels, video_id = data  # inputs: b, c, t, h, w
 
         per_frame_logits = i3d(inputs)
@@ -116,40 +96,14 @@ def test_i3d(
         out_probs = np.sort(predictions.cpu().detach().numpy()[0])
 
         data_class_num = labels[0].nonzero()[0][0].item()
-        print("Actual class: {}".format(data_class_num))
-        print("Predicted class: {}".format(torch.argmax(predictions[0]).item()))
-
-        # Top-5 accuracy
-        if data_class_num in out_labels[-5:]:
-            correct_5 += 1
-            top5_tp[data_class_num] += 1
-        else:
-            top5_fp[data_class_num] += 1
-
-        # Top-10 accuracy
-        if data_class_num in out_labels[-10:]:
-            correct_10 += 1
-            top10_tp[data_class_num] += 1
-        else:
-            top10_fp[data_class_num] += 1
 
         # Top-1 accuracy
         if torch.argmax(predictions[0]).item() == data_class_num:
-            correct += 1
-            top1_tp[data_class_num] += 1
-        else:
-            top1_fp[data_class_num] += 1
+            correct = correct + 1
+            correct_vids.append(video_id)
 
-        print(video_id, float(correct) / len(val_dataloader),
-              float(correct_5) / len(val_dataloader),
-              float(correct_10) / len(val_dataloader))
-
-    # per-class accuracy
-    top1_per_class = np.mean(top1_tp / (top1_tp + top1_fp))
-    top5_per_class = np.mean(top5_tp / (top5_tp + top5_fp))
-    top10_per_class = np.mean(top10_tp / (top10_tp + top10_fp))
-    print('top-k average per class acc: {}, {}, {}'.format(top1_per_class, top5_per_class, top10_per_class))
-
+    print(correct_vids)
+    print(correct, "/", count)
 
 def test_i3d_cli(argv) -> None:
     """
