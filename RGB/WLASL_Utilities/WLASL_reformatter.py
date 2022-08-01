d440d6dbce14a3d9ed13d536736189ce1493be78
Isabelle <isabelle.y.johns@gmail.com>
2022-08-02 01:19:17 +1200

    add comments

--- RGB/WLASL_Utilities/WLASL_reformatter.py
+++ RGB/WLASL_Utilities/WLASL_reformatter.py
@@ -2,10 +2,16 @@ import json
 import shutil
 import os
 
+# The json file to generate the new dataset from.
 WLASL_JSON_FILE = "/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL2000/data/splits/asl100.json"
+
+# The file you'd like the class JSON array to be written to.
 WLASL_CLASSES = "/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL_Utilities/WLASL_classes.json"
 
+# The desired directory for the new reformatted WLASL.
 NEW_WLASL_LOCATION = "/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL-100"
+
+# The location of the existing WLASL videos.
 EXISTING_WLASL_LOCATION = "/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL2000"
 
 def write_classes():
