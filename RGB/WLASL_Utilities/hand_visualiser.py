5ca3fe744e4ec681a155246aa94311b5d5ab9c59
Isabelle <isabelle.y.johns@gmail.com>
2022-08-24 21:02:33 +1200

    successful hand and pose estimation

--- RGB/WLASL_Utilities/hand_visualiser.py
+++ RGB/WLASL_Utilities/hand_visualiser.py
@@ -3,12 +3,12 @@ import mmcv
 from mmpose.apis import (inference_top_down_pose_model, init_pose_model, vis_pose_result, process_mmdet_results)
 from mmdet.apis import inference_detector, init_detector
 
-pose_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py'
-pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth'
-det_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
-det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+hand_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/configs/hand/2d_kpt_sview_rgb_img/topdown_heatmap/coco_wholebody_hand/res50_coco_wholebody_hand_256x256.py'
+hand_checkpoint = 'https://download.openmmlab.com/mmpose/hand/resnet/res50_coco_wholebody_hand_256x256-8dbc750c_20210908.pth'
+det_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/demo/mmdetection_cfg/cascade_rcnn_x101_64x4d_fpn_1class.py'
+det_checkpoint = 'https://download.openmmlab.com/mmpose/mmdet_pretrained/cascade_rcnn_x101_64x4d_fpn_20e_onehand10k-dac19597_20201030.pth'
 
-pose_model = init_pose_model(pose_config, pose_checkpoint)
+pose_model = init_pose_model(hand_config, hand_checkpoint)
 det_model = init_detector(det_config, det_checkpoint)
 
 vid = mmcv.VideoReader("/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL-100/train/0001024.mp4")
