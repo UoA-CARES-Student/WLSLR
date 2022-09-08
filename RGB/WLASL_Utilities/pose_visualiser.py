81c5420358cfd3f86028e8d60bb37ce92833f595
Isabelle <isabelle.y.johns@gmail.com>
2022-09-08 18:04:04 +1200

    working keypoint generation in WLASL JSON format

--- RGB/WLASL_Utilities/pose_visualiser.py
+++ RGB/WLASL_Utilities/pose_visualiser.py
@@ -1,26 +1,11 @@
 import json
+import os
 
-import cv2
+import traceback
 import mmcv
-import numpy
+from datetime import datetime
 from mmpose.apis import (inference_top_down_pose_model, init_pose_model, vis_pose_result, process_mmdet_results)
 from mmdet.apis import inference_detector, init_detector
-
-pose_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco' \
-              '/hrnet_w48_coco_256x192.py'
-pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth'
-pose_det_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
-pose_det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco' \
-                      '/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-
-hand_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/configs/hand/2d_kpt_sview_rgb_img/topdown_heatmap' \
-              '/coco_wholebody_hand/hrnetv2_w18_coco_wholebody_hand_256x256.py'
-hand_checkpoint = 'https://download.openmmlab.com/mmpose/hand/hrnetv2/hrnetv2_w18_coco_wholebody_hand_256x256' \
-                  '-1c028db7_20210908.pth'
-hand_det_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/demo/mmdetection_cfg' \
-                  '/cascade_rcnn_x101_64x4d_fpn_1class.py'
-hand_det_checkpoint = 'https://download.openmmlab.com/mmpose/mmdet_pretrained' \
-                      '/cascade_rcnn_x101_64x4d_fpn_20e_onehand10k-dac19597_20201030.pth'
 
 wholebody_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/configs/wholebody/2d_kpt_sview_rgb_img' \
                    '/topdown_heatmap/coco-wholebody/hrnet_w48_coco_wholebody_384x288.py'
@@ -30,17 +15,39 @@ wholebody_det_config = '/home/izzy/Documents/UoA/Sem_1_2022/P4P/mmpose/demo/mmde
 wholebody_det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco' \
                            '/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
+DATASET_BASE = "/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL-100"
 
-def predict_and_visualise(model, det_model, input_vid, output_vid):
-    fps = input_vid.fps
-    size = (input_vid.width, input_vid.height)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video_writer = cv2.VideoWriter("/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/pose/test_vis_output/" + output_vid,
-                                   fourcc,
-                                   fps,
-                                   size)
+def predict_and_visualise(model,
+                          det_model,
+                          input_vid_path,
+                          # output_vid
+                          ):
+    input_vid = mmcv.VideoReader(input_vid_path)
+    # fps = input_vid.fps
+    # size = (input_vid.width, input_vid.height)
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    # video_writer = cv2.VideoWriter("/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/pose/test_vis_output/" + output_vid,
+    #                                fourcc,
+    #                                fps,
+    #                                size)
 
+    offset = 0
+
+    video_name = os.path.basename(input_vid_path)
+    split_name = input_vid_path.split(os.sep)[-2]
+
+    print(
+        f"Now processing video {video_name} from the {split_name} directory")
     for frame_id, cur_frame in enumerate(mmcv.track_iter_progress(input_vid)):
+        path_dir = f"{DATASET_BASE}/{split_name}/data/pose_per_individual_videos/{os.path.splitext(video_name)[0]}"
+
+        frame_num = frame_id - offset
+
+        if not os.path.exists(path_dir):
+            os.mkdir(path_dir)
+
+        output_json = {}
+
         # inference detection
         mmdet_results = inference_detector(det_model, cur_frame)
 
@@ -55,59 +62,79 @@ def predict_and_visualise(model, det_model, input_vid, output_vid):
             format='xyxy',
             dataset=model.cfg.data.test.type)
 
-        # get just wholebody prediction
-        for _ in pose_results:
-            while len(pose_results) > 1:
-                pose_results.pop(1)
-
-        # cull unneeded keypoints:
-        # Keypoints 1 - 13: upper body
-        # Keypoints 92 - 112: right hand
-        # Keypoints 113 - 133: left hand
-        index = 1
-        for _ in pose_results[0]['keypoints']:
-            if 13 < index < 92:
-                pose_results[0]['keypoints'][index - 1] = [0, 0, 0]
-            index += 1
-
-        if frame_id == 1:
-            index = 0
+        if len(pose_results) > 0:
+            # get just wholebody prediction
             for _ in pose_results:
-                pose_results[index].update({'bbox': numpy.ndarray.tolist(pose_results[index].get('bbox'))})
-                pose_results[index].update({'keypoints': numpy.ndarray.tolist(pose_results[index].get('keypoints'))})
-                index += 1
+                while len(pose_results) > 1:
+                    pose_results.pop(1)
 
-            with open("data/pose_test.json", "w") as outfile:
-                json.dump(pose_results, outfile)
+            # cull unneeded keypoints:
+            # Keypoints 1 - 13: upper body
+            # Keypoints 92 - 112: right hand
+            # Keypoints 113 - 133: left hand
+            pose_keypoints_2d = []
+            hand_left_keypoints_2d = []
+            hand_right_keypoints_2d = []
 
+            index = 1
 
+            try:
+                for _ in pose_results[0]['keypoints']:
+                    if 13 < index < 92:
+                        pose_results[0]['keypoints'][index - 1] = [0, 0, 0]
+                    elif index <= 13:
+                        pose_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][0]))
+                        pose_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][1]))
+                        pose_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][2]))
+                    elif 92 <= index <= 112:
+                        hand_left_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][0]))
+                        hand_left_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][1]))
+                        hand_left_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][2]))
+                    else:
+                        hand_right_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][0]))
+                        hand_right_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][1]))
+                        hand_right_keypoints_2d.append(float(pose_results[0]['keypoints'][index - 1][2]))
+                    index += 1
+            except Exception as e:
+                traceback.print_exc()
+                print(f"Error occurred on frame {frame_id}")
+                with open('/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/WLASL_Utilities/log.txt', 'a') as logfile:
+                    logfile.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+                    logfile.write(f" Error occurred on frame {frame_id}, video {os.path.splitext(video_name)[0]}, split {split_name}\n")
+                    logfile.write(str(e))
+                    logfile.write(traceback.format_exc())
+                    logfile.write("--------------------------------\n")
 
-        # show pose estimation results
-        vis_result = vis_pose_result(
-            model,
-            cur_frame,
-            pose_results,
-            dataset=model.cfg.data.test.type,
-            show=False)
-        # reduce image size
-        video_writer.write(vis_result)
+            output_json["pose_keypoints_2d"] = pose_keypoints_2d
+            output_json["hand_left_keypoints_2d"] = hand_left_keypoints_2d
+            output_json["hand_right_keypoints_2d"] = hand_right_keypoints_2d
 
-    video_writer.release()
+            with open(f"{path_dir}/image_{frame_num:05}_keypoints.json", "w") as outfile:
+                json.dump(output_json, outfile)
+
+        #     # show pose estimation results
+        #     vis_result = vis_pose_result(
+        #         model,
+        #         cur_frame,
+        #         pose_results,
+        #         dataset=model.cfg.data.test.type,
+        #         show=False)
+        #     # reduce image size
+        #     video_writer.write(vis_result)
+        #
+        # video_writer.release()
+
+        else:
+            offset += 1
 
 
 if __name__ == "__main__":
-    # pose_model = init_pose_model(pose_config, pose_checkpoint)
-    # pose_det_model = init_detector(pose_det_config, pose_det_checkpoint)
-    #
-    # hand_model = init_pose_model(hand_config, hand_checkpoint)
-    # hand_det_model = init_detector(hand_det_config, hand_det_checkpoint)
-
     wholebody_model = init_pose_model(wholebody_config, wholebody_checkpoint)
     wholebody_det_model = init_detector(wholebody_det_config, wholebody_det_checkpoint)
 
-    # vid = mmcv.VideoReader("/home/izzy/Documents/UoA/Sem_1_2022/P4P/WLSLR/RGB/MS-ASL-100/train/0002008.mp4")
-    vid = mmcv.VideoReader("/home/izzy/Downloads/video-14611836-56f0-44ed-9c4e-44576ccb6572-1662074039.mp4")
+    for split_dir in os.listdir(DATASET_BASE):
+        files = os.listdir(os.path.join(DATASET_BASE, split_dir))
 
-    # predict_and_visualise(pose_model, pose_det_model, vid, "pose_test.mp4")
-    # predict_and_visualise(hand_model, hand_det_model, vid, "hand_test.mp4")
-    predict_and_visualise(wholebody_model, wholebody_det_model, vid, "wholebody_test.mp4")
+        for file in files:
+            if file.endswith(".mp4"):
+                predict_and_visualise(wholebody_model, wholebody_det_model, os.path.join(DATASET_BASE, split_dir, file))
